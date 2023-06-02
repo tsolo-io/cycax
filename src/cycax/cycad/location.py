1ed5e9c842fa64a905a4d5cfdbc72492ea36885e
Helen Forbes <helen@forbes.capetown>
2023-06-02 13:00:57 +0200

    Added doc comments to the location file

--- src/cycax/cycad/location.py
+++ src/cycax/cycad/location.py
@@ -1,4 +1,4 @@
-#global location variables
+# global location variables
 LEFT = "LEFT"
 RIGHT = "RIGHT"
 TOP = "TOP"
@@ -10,12 +10,10 @@ BACK = "BACK"
 class Location:
     """This class will define the loation of an object in 3D space."""
 
-
     def __init__(self, x: float, y: float, z: float, side: str):
-        
         """
-        This method will initialize a location along the x, y, z axis. 
-        
+        This method will initialize a location along the x, y, z axis.
+
         Args:
             x : The location of x along the x axis.
             y : The location of y along the y axis.
@@ -30,12 +28,11 @@ class Location:
     def __repr__(self):
         """
         Returns a location in a string format to be used for debugging.
-        
+
         Returns:
-            str: this method will return a string which will specify the current location of an object. 
+            str: this method will return a string which will specify the current location of an object.
         """
         return f"x={self.x} y={self.y} z={self.z} side={self.side}"
-
 
     def move(self, x: float = 0, y: float = 0, z: float = 0):
         """Update position relative to its current location.
@@ -51,7 +48,7 @@ class Location:
 
     def at(self, x: float = 0, y: float = 0, z: float = 0):
         """This class will move a location to the exact location provided.
-        
+
         Args:
             x: Move to this exact location on the X-axis.
             y: Move to this exact location on the Y-axis.
@@ -63,11 +60,11 @@ class Location:
 
     def swap_xy(self, rot: float, max_y: float):
         """Rotate while holding the top where it currenly is.
-        
+
         Args:
             rot: This will specify the number of times the swap is to be performed. This allows for it to be easier to move the objsect 180. as you will not need to call the method twice.
             max_y: This is the maximium value of the object on the y axis. This is used as a metric to move the value once swapped back into the quadrant it came from. In this manner if performs bot a rotate and a translate back to its original quadrant.
-            
+
         """
         while rot != 0:
             self.y, self.x = self.x, max_y - self.y
@@ -76,11 +73,11 @@ class Location:
 
     def swap_xz(self, rot: float, max_x: float):
         """Rotate while holding the front where it currenly is.
-        
+
         Args:
             rot: This will specify the number of times the swap is to be performed. This allows for it to be easier to move the objsect 180. as you will not need to call the method twice.
             max_x: This is the maximium value of the object on the x axis. This is used as a metric to move the value once swapped back into the quadrant it came from. In this manner if performs bot a rotate and a translate back to its original quadrant.
-            
+
         """
         while rot != 0:
             self.x, self.z = self.z, max_x - self.x
@@ -89,11 +86,11 @@ class Location:
 
     def swap_yz(self, rot: float, max_z: float):
         """Rotate while holding the left where it currenly is.
-        
+
         Args:
             rot: This will specify the number of times the swap is to be performed. This allows for it to be easier to move the objsect 180. as you will not need to call the method twice.
             max_z: This is the maximium value of the object on the z axis. This is used as a metric to move the value once swapped back into the quadrant it came from. In this manner if performs bot a rotate and a translate back to its original quadrant.
-            
+
         """
         while rot != 0:
             self.y, self.z = max_z - self.z, self.y
