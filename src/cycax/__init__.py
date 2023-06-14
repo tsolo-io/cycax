acd9fe5a4413a701a6f8615928caf4a04751a1ad
Martin Slabber <martin@tsolo.io>
2023-06-14 11:58:33 +0200

    Added modules to __init__ to save typing when using cycax.

--- src/cycax/__init__.py
+++ src/cycax/__init__.py
@@ -3,4 +3,5 @@ from cycax.cycad.cuboid import Cuboid  # noqa: F401
 from cycax.cycad.external_part import ExternalPart  # noqa: F401
 from cycax.cycad.assembly import Assembly  # noqa: F401
 from cycax.cycad.sheet_metal import SheetMetal  # noqa: F401
+
 # SPDX-License-Identifier: Apache-2.0
