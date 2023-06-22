f7d136f66a7ea84c3586c3e2b8bf34118d77105f
Helen Forbes <helen@forbes.capetown>
2023-06-22 13:50:39 +0200

    added a test to test multiple rotates

--- tests/test_part_multiple_rotate.py
+++ tests/test_part_multiple_rotate.py
@@ -10,22 +10,73 @@ def test_rotate():
 
     assembly = Assembly("assembly-test")
     mypart1 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test1")
+    mypart2 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test2")
+    mypart3 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test3")
+    mypart4 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test4")
+    mypart5 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test5")
+    mypart6 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test6")
+    mypart7 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test7")
+    mypart8 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test8")
+    mypart9 = SheetMetal(x_size=length, y_size=width, z_size=thickness, part_no="part-test9")
     assembly.add(mypart1)
+    assembly.add(mypart2)
+    assembly.add(mypart3)
+    assembly.add(mypart4)
+    assembly.add(mypart5)
+    assembly.add(mypart6)
+    assembly.add(mypart7)
+    assembly.add(mypart8)
+    assembly.add(mypart9) 
+
 
     # Test that a part is rotated multiple times.
-        assembly.rotateFreezeTop(mypart1)
-        assembly.rotateFreezeLeft(mypart1)
-        assembly.rotateFreezeFront(mypart1)
-        assembly_def = assembly.export()
-        assert assembly_def[0]["rotate"] == [0, 0, 0]
-        assert assembly_def[1]["rotate"] == [angle % 360, 0, 0]
-        assert assembly_def[2]["rotate"] == [0, angle % 360, 0]
-        assert assembly_def[3]["rotate"] == [0, 0, angle % 360]
-
-    # Test that after being rotate full circle, twice, that the part is still the same.
-    for i in range(1, 4):
-        assert assembly_def[0]["rotate"] == assembly_def[i]["rotate"]
-        assert assembly_def[0]["rotmax"] == assembly_def[i]["rotmax"]
+    assembly.rotateFreezeLeft(mypart1)
+    assembly.rotateFreezeFront(mypart1)
+    assembly.rotateFreezeTop(mypart1)
+    assembly.rotateFreezeLeft(mypart1)
+    assembly.rotateFreezeLeft(mypart1)
+    
+    assembly.rotateFreezeLeft(mypart2)
+    assembly.rotateFreezeFront(mypart2)
+    assembly.rotateFreezeTop(mypart2)
+    
+    assembly.rotateFreezeLeft(mypart3)
+    assembly.rotateFreezeFront(mypart3)
+    
+    assembly.rotateFreezeFront(mypart4)
+    assembly.rotateFreezeLeft(mypart4)
+    
+    assembly.rotateFreezeTop(mypart5)
+    assembly.rotateFreezeFront(mypart5)
+    
+    assembly.rotateFreezeFront(mypart6)
+    assembly.rotateFreezeTop(mypart6)
+    
+    assembly.rotateFreezeLeft(mypart7)
+    assembly.rotateFreezeTop(mypart7)
+    
+    assembly.rotateFreezeTop(mypart8)
+    assembly.rotateFreezeLeft(mypart8)
+    
+    assembly.rotateFreezeLeft(mypart9)
+    assembly.rotateFreezeFront(mypart9)
+    assembly.rotateFreezeTop(mypart9)
+    assembly.rotateFreezeLeft(mypart9)
+    assembly.rotateFreezeFront(mypart9)
+    assembly.rotateFreezeTop(mypart9)
+    
+    
+    
+    assembly_def = assembly.export()
+    assert assembly_def[0]["rotate"] == [180, 0, 270]
+    assert assembly_def[1]["rotate"] == [180, 0, 90]
+    assert assembly_def[2]["rotate"] == [90, 0, 90]
+    assert assembly_def[3]["rotate"] == [0, 90, 90]
+    assert assembly_def[4]["rotate"] == [90, 0, 90]
+    assert assembly_def[5]["rotate"] == [90, 90, 0]
+    assert assembly_def[6]["rotate"] == [90, 90, 0]
+    assert assembly_def[7]["rotate"] == [0, 90, 90]
+    assert assembly_def[8]["rotate"] == [270, 0, 270]
 
 
 # TODO: Add tests that rotate the part in multiple directions.
