a329b88d8753778db92c463db29a2895fadd3a92
Helen Forbes <helen@forbes.capetown>
2023-08-07 17:19:57 +0200

    updated .blender to .blend

--- src/cycax/cycad/assembly_blender.py
+++ src/cycax/cycad/assembly_blender.py
@@ -55,7 +55,7 @@ class AssemblyBlender:
         """Used to help rotate the object on the spot while freezing the front"""
         while rot != 0:
             max_z = rotmax[2]
-            rotation[0], rotation[2] = max_z -rotation[2], rotation[0]
+            rotation[0], rotation[2] = max_z - rotation[2], rotation[0]
             rotmax[0], rotmax[2] = rotmax[2], rotmax[0]
             rot = rot - 1
         return rotation, rotmax
@@ -81,16 +81,16 @@ class AssemblyBlender:
         rotation = [0, 0, 0]
         for item in rotate:
             if item["axis"] == "x":
-                bpy.ops.transform.rotate(value=math.radians(360-90), orient_axis="X")
+                bpy.ops.transform.rotate(value=math.radians(360 - 90), orient_axis="X")
                 working = self.swap_yz_(rotation, 3, rotmax)
-            #blender does clockwise instead of anticlockwise rotations as we would expect in openscad.
+            # blender does clockwise instead of anticlockwise rotations as we would expect in openscad.
 
             if item["axis"] == "y":
-                bpy.ops.transform.rotate(value=math.radians(360-90), orient_axis="Y")
+                bpy.ops.transform.rotate(value=math.radians(360 - 90), orient_axis="Y")
                 working = self.swap_xz_(rotation, 3, rotmax)
 
             if item["axis"] == "z":
-                bpy.ops.transform.rotate(value=math.radians(360-90), orient_axis="Z")
+                bpy.ops.transform.rotate(value=math.radians(360 - 90), orient_axis="Z")
                 working = self.swap_xy_(rotation, 3, rotmax)
 
             rotation = working[0]
@@ -110,7 +110,7 @@ class AssemblyBlender:
         template_object = bpy.data.objects.get(working_part)
         colour_rgb = colour_to_rgb[colour.upper()]
         matcolour = bpy.data.materials.new(colour)
-        matcolour.diffuse_color = (colour_rgb[0]/255,colour_rgb[1]/255,colour_rgb[2]/255,0.8)
+        matcolour.diffuse_color = (colour_rgb[0] / 255, colour_rgb[1] / 255, colour_rgb[2] / 255, 0.8)
 
         template_object.active_material = matcolour
 
@@ -130,6 +130,6 @@ class AssemblyBlender:
             self.colour(action["colour"], action["part_no"])
 
         logging.info("Saving the .blend file.")
-        save_file = str(self._base_path / f"{self.part_no}.blender")
+        save_file = str(self._base_path / f"{self.part_no}.blend")
         bpy.ops.wm.save_as_mainfile(filepath=save_file)
         # bpy.ops.wm.save_mainfile()
