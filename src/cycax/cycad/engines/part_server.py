c1b7ef67bbd1fa8c04e592805c3b88c604310800
Martin Slabber <martin@tsolo.io>
2025-01-03 13:51:55 +0200

    Improve the part_server engine.

--- src/cycax/cycad/engines/part_server.py
+++ src/cycax/cycad/engines/part_server.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import time
 
 import httpx
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 from cycax.cycad.engines.base_part_engine import PartEngine
 
@@ -14,13 +16,23 @@ class PartEngineServer(PartEngine):
     jobs = {}
 
     def connect(self, address: str | None = None) -> httpx.Client:
-        if not hasattr("_client", self):
+        if not hasattr(self, "_client"):
             self._client = None
         if self._client is None:
             if address is None:
                 address = os.environ["CYCAX_SERVER"]
             self._client = httpx.Client(base_url=address)
         return self._client
+
+    @retry(reraise=True, stop=stop_after_attempt(13), wait=wait_fixed(2))
+    def server_get_job(self, job_id: str) -> dict:
+        client = self.connect()
+        logging.info("Get info for Job %s", job_id)
+        reply = client.get(f"/jobs/{job_id}")
+        job = reply.json().get("data")
+        state = job["attributes"]["state"]["job"]
+        assert state == "COMPLETED"
+        return job
 
     def check_part_init(self):
         """Early hook for part classes to do custom checks."""
@@ -31,11 +43,14 @@ class PartEngineServer(PartEngine):
         client = self.connect()
         response = client.post("/jobs", json=spec)
         response.raise_for_status()
-        self.jobs[part.part_no] = response.json()
+        self.jobs[part.part_no] = response.json().get("data")
 
     def build(self, part) -> list:
         """Create the output files for the part."""
 
         logging.error(self.jobs[part.part_no])
+        job_id = self.jobs[part.part_no]["id"]
+        job = self.server_get_job(job_id)
+        logging.info(job)
         _files = []
         return self.file_list(files=_files, engine="OpenSCAD", score=3)
