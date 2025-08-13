ac515b0946e842deb837b96cd98b6b391379b9eb
Martin Slabber <martin@tsolo.io>
2025-08-13 15:09:32 +0200

    feat: Slots demo.

--- src/examples/slots.py
+++ src/examples/slots.py
@@ -8,6 +8,7 @@ from cycax.cycad.assembly import Assembly
 from cycax.cycad.cuboid import Cuboid, Print3D, SheetMetal
 from cycax.cycad.engines.assembly_build123d import AssemblyBuild123d
 from cycax.cycad.engines.part_build123d import PartEngineBuild123d
+from cycax.cycad.engines.part_freecad import PartEngineFreeCAD
 from cycax.cycad.location import BACK, BOTTOM, FRONT, LEFT, RIGHT, SIDES, TOP
 
 
@@ -15,20 +16,26 @@ class Cube(Cuboid):
     def __init__(self, side: str, *, horizontal: bool = True):
         self.side_name = side.lower()
         self.hrz = horizontal
-        super().__init__(part_no=f"cube-{self.side_name}", x_size=100, y_size=100, z_size=100)
+        if horizontal:
+            self.part_no = f"cube-{self.side_name}-horizontal"
+        else:
+            self.part_no = f"cube-{self.side_name}-vertical"
+        name = self.part_no
+        super().__init__(part_no=name, x_size=100, y_size=100, z_size=100)
         self.calculate()
+        if self.hrz:
+            self.colour = "blue"
 
     def calculate(self):
         diameter = 6
         side = getattr(self, self.side_name)
+        if self.hrz:
+            pos = (diameter, self.y_size / 2)
+        else:
+            pos = (self.x_size / 2, diameter)
+        side.slot(pos=pos, width=diameter, length=self.x_size - 2 * diameter, horizontal=self.hrz)
         side.slot(
-            pos=(diameter, self.y_size / 2), width=diameter, length=self.x_size - 2 * diameter
-        )
-        side.slot(
-            pos=(diameter, self.y_size / 2),
-            width=diameter,
-            length=self.x_size - 2 * diameter,
-            external_subtract=True,
+            pos=pos, width=diameter, length=self.x_size - 2 * diameter, external_subtract=True, horizontal=self.hrz
         )
         side.hole(pos=(self.x_size / 2, self.y_size / 2), diameter=diameter * 2, depth=5)  # Marker, should point down
 
@@ -44,7 +51,7 @@ def main():
         rotation_str = {LEFT: "yyy", RIGHT: "y", FRONT: "yx", BACK: "xzyy", TOP: "zyy", BOTTOM: "z"}[side]
         for horizontal in (True, False):
             for subtract in (True, False):
-                cube = Cube(side, horizontal=horizontal)
+                cube = Cube(side=side, horizontal=horizontal)
                 assembly.add(cube, f"cube-{side}-{horizontal}-{subtract}")
                 if subtract:
                     cube.rotate(rotation_str)
@@ -59,10 +66,9 @@ def main():
                 if subtract:
                     cube.level(bottom=base_board.top, subtract=True)
 
-
     assembly.save(f"./build/{name}")
     # assembly.render()
-    assembly.build(engine=AssemblyBuild123d(name), part_engines=[PartEngineBuild123d()])
+    assembly.build(engine=AssemblyBuild123d(name), part_engines=[PartEngineFreeCAD(), PartEngineBuild123d()])
 
 
 if __name__ == "__main__":
