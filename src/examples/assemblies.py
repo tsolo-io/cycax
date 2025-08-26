f6376c540b4aeab0fe6494cba7e05a0f592533d5
Martin Slabber <martin@tsolo.io>
2025-08-26 09:08:35 +0200

    fix: Improve the assemblies example.

--- src/examples/assemblies.py
+++ src/examples/assemblies.py
@@ -38,43 +38,59 @@ class Device(Cuboid):
     def definition(self):
         """Define the device."""
         for side in (self.left, self.right):
-            for x in range(10, 50, 90):
+            for x in (12, 30, 60):
                 side.hole(pos=(x, 10), diameter=4.2, depth=2)
                 side.hole(pos=(x, 10), diameter=3.9, depth=10)
                 side.hole(pos=(x, 10), diameter=4.2, external_subtract=True)  # Through everything
 
 
-def assemble() -> Assembly:
-    box = Assembly("box")
+class DriveStack(Assembly):
+    def definition(self):
+        """Define the drive stack."""
+        bracket_l = Bracket()
+        bracket_r = Bracket()
+        self.add(bracket_l, "bracket_left")
+        self.add(bracket_r, "bracket_right")
 
-    bottom = SheetMetal(x_size=100, y_size=100, z_size=2, part_no="bottom")
-    box.add(bottom)
+        dev1 = Device()
+        dev2 = Device()
+        self.add(dev1, "device1")
+        self.add(dev2, "device2")
+
+        bracket_l.rotate("xz")
+        bracket_r.rotate("xz")
+
+        dev2.level(bottom=dev1.top, left=dev1.left, front=dev1.front)
+        dev2.move(z=10)
+
+        bracket_l.level(top=dev2.top, right=dev2.left, front=dev2.front)
+        bracket_r.level(top=dev2.top, left=dev2.right, front=dev2.front)
+        dev1.level(right=bracket_r.left, subtract=True)
+        dev1.level(left=bracket_l.right, subtract=True)
+        dev2.level(right=bracket_r.left, subtract=True)
+        dev2.level(left=bracket_l.right, subtract=True)
+
+
+def assemble() -> Assembly:
+    base = Assembly("base")
+    bottom = SheetMetal(x_size=200, y_size=200, z_size=2, part_no="bottom")
+    base.add(bottom)
+
+    box = Assembly("box")
+    box.add_assembly(base)
 
     # Below will be moved to an assembly.
-    bracket_l = Bracket()
-    bracket_r = Bracket()
-    box.add(bracket_l)
-    box.add(bracket_r)
 
-    dev1 = Device()
-    dev2 = Device()
-    box.add(dev1)
-    box.add(dev2)
+    drive_stack = DriveStack(name="DriveStack")
+    drive_stack.definition()  # TODO: Assembly should call this automatically.
+    box.add_assembly(drive_stack)
 
-    bracket_l.rotate("xz")
-    bracket_r.rotate("xzzz")
+    # TODO: Change to: drive_stack.level(bottom=base.top, left=base.left, front=base.front)
+    drive_stack.bottom.level(base.top)
+    drive_stack.front.level(base.front)
+    drive_stack.left.level(base.left)
 
-    dev2.level(bottom=dev1.top, left=dev1.left, front=dev1.front)
-    dev2.move(z=10)
-
-    bracket_l.level(top=dev2.top, right=dev2.left, front=dev2.front)
-    bracket_r.level(top=dev2.top, left=dev2.right, front=dev2.front)
-    dev1.level(right=bracket_r.left, subtract=True)
-    dev1.level(left=bracket_l.right, subtract=True)
-    dev2.level(right=bracket_r.left, subtract=True)
-    dev2.level(left=bracket_l.right, subtract=True)
-
-    return box
+    return box.combine_all_assemblies()
 
 
 if __name__ == "__main__":
