15ec7cc58ed7540fc7cdf0834232a5158b62b970
Martin Slabber <martin@tsolo.io>
2025-10-01 15:11:38 +0200

    Remove unused functions.

--- src/cycax/cli/tui.py
+++ src/cycax/cli/tui.py
@@ -1,7 +1,6 @@
 """Text User Interface."""
 
 import logging
-from datetime import datetime, timezone
 
 import rich
 import typer
@@ -9,42 +8,6 @@ from rich.console import Console
 from rich.table import Table
 
 console = Console()
-
-SAMPLE_KEYS = ["time", "status", "value", "value_ts"]
-
-
-def sample_table_row(row: list[any]) -> list[str]:
-    values = []
-    for key in SAMPLE_KEYS:
-        if key == "status":
-            _value = str(row[key])
-            style = {"warn": "[red]", "nominal": "[green]"}.get(_value, "[blue]")
-            value = f"{style}{_value}"
-        elif key in ("time", "value_ts"):
-            _value = float(row[key])
-            sample_dt = datetime.fromtimestamp(_value, tz=timezone.utc)
-            time_str = sample_dt.isoformat()
-            value = f"{_value} ({time_str})"
-        else:
-            value = str(row[key])
-        values.append(value)
-    return values
-
-
-def samples_table(data: list[dict], title: str):
-    title_keys = [k.title().replace("_", " ") for k in SAMPLE_KEYS]
-    table = Table(*title_keys, title=title)
-    for row in data:
-        values = sample_table_row(row)
-        table.add_row(*values)
-    console.print(table)
-
-
-def print_samples(ctx: typer.Context, data: list[dict], title: str):
-    if ctx.obj.json:
-        rich.print_json(data=data)
-    else:
-        samples_table(data=data, title=title)
 
 
 def print_table(ctx: typer.Context, data: list[dict], title: str):
